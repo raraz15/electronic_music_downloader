674126b7fe1f69aa99891f8e876d4aef718f29e7
Recep Oguz Araz <oguza97@gmail.com>
2022-09-28 23:59:56 +0200

    analysis, plotting

--- beatport_crawler/analyze_beatport.py
+++ beatport_crawler/analyze_beatport.py
@@ -7,10 +7,10 @@ import datetime as dt
 import re
 import requests
 import argparse
-
 from bs4 import BeautifulSoup
+from collections import defaultdict
 
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 OUTPUT_DIR="Charts"
 DATE=dt.datetime.strftime(dt.datetime.now(),"%d_%m_%Y")
@@ -198,3 +198,67 @@ if __name__ == '__main__':
         artists=tracks[i]['Artist(s)']
         print(f"| {i+1:>2} | {title:<{max_title_len}} | {artists:<{max_artist_len}} |")
     print("-"*(3+3+2+3+1+max_title_len+max_artist_len))
+
+    # Analysis
+    key_dict,bpm_dict,label_dict,artist_dict = defaultdict(int),defaultdict(int),defaultdict(int),defaultdict(int)
+    remix_dict = {'remix': 0, 'original': 0}
+    for track in tracks.values():
+        bpm_dict[track['BPM']] += 1
+        key_dict[track['Key']] += 1
+        label_dict[track['Label']] += 1
+        for artist in track['Artist(s)'].split(','):
+            artist_dict[artist] += 1             
+        if 'Remixer(s)' in track:
+            remix_dict['remix'] += 1
+        else:
+            remix_dict['original'] += 1
+    artist_dict = dict(sorted(artist_dict.items()))
+    key_dict = dict(sorted(key_dict.items()))               
+    bpm_dict = dict(sorted(bpm_dict.items()))
+    label_dict = dict(sorted(label_dict.items()))
+
+    # Plotting            
+    # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+    labels = ['Remix', 'Original']
+    explode = (0, 0.05)  
+    fig, ax = plt.subplots(figsize=(10,5))
+    ax.pie(remix_dict.values(), explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90, textprops={'fontsize': 15})
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.draw()   
+
+    fig, ax = plt.subplots(figsize=(20,8))
+    ax.bar(artist_dict.keys(), artist_dict.values())
+    ax.set_ylabel('Number of Appearances',fontsize=15)
+    ax.set_xlabel('Artist',fontsize=15)
+    ax.set_title('Beatport TOP100 Artist Distribution',fontsize=14)
+    plt.xticks(rotation = 90)
+    #plt.savefig("Beatport TOP100 Artist Distribution.png")
+    plt.draw()    
+
+    fig, ax = plt.subplots(figsize=(20,8))
+    ax.bar(key_dict.keys(), key_dict.values())
+    ax.set_ylabel('Number of Tracks',fontsize=15)
+    ax.set_xlabel('Track Key',fontsize=15)
+    ax.set_title('Beatport TOP100 Key Distribution',fontsize=14)
+    #plt.savefig("Tech House Key Distribution.png")
+    plt.draw()  
+
+    fig, ax = plt.subplots(figsize=(20,8))
+    ax.bar(bpm_dict.keys(), bpm_dict.values())
+    ax.set_ylabel('Number of Tracks',fontsize=15)
+    ax.set_xlabel('Track BPM',fontsize=15)
+    ax.set_title('Beatport TOP100 BPM Distribution',fontsize=14)
+    #plt.savefig("Tech House BPM Distribution.png")
+    plt.draw()   
+
+    fig, ax = plt.subplots(figsize=(20,8))
+    ax.bar(label_dict.keys(), label_dict.values())
+    ax.set_ylabel('Number of Tracks',fontsize=15)
+    ax.set_xlabel('Label Name',fontsize=15)
+    ax.set_title('Beatport TOP100 Label Distribution',fontsize=14)
+    plt.xticks(rotation = 90)
+    #plt.savefig("Tech House Label Distribution.png")
+    plt.draw()
+
+    plt.show()        
