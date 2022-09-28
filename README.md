ef793c399a8ad6946871ba792a41eb064d99bdb9
Recep Oguz Araz <oguza97@gmail.com>
2022-09-28 16:41:00 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # electronic_music_downloader
+
 A repository for downloading high quality electronic music from YouTube with Spotify and Beatport crawling capacities.
+
+1) Create the Environment:
+
+    conda create --name myenv --file requirements.txt
+
+2) You will need the ffmpeg for youtube-dl.
+
+    For MacOS: 
+
+        brew install ffmpeg
+
+    For Linux:
+
+        sudo apt-get install ffmpeg
+
+3) Activate the environment
+
+    conda activate myenv
+
+4) How to use
+
+    python mp3_downloader.py -l="youtube_link"
---
+++ mp3_downloader/mp3_downloader.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import argparse
+import datetime as dt
+
+import youtube_dl
+
+OUTPUT_DIR = f"Downloads/{dt.datetime.strftime(dt.datetime.now(),'%d_%m_%y')}"
+SIMPLE_FORMAT=f"%(title)s.%(ext)s"
+YDL_OPTS = {
+		'format': 'bestaudio/best',
+		'postprocessors': [{'key': 'FFmpegExtractAudio',
+							'preferredcodec': 'mp3',
+							'preferredquality': '320'}],
+		'outtmpl':  f"{OUTPUT_DIR}/{SIMPLE_FORMAT}",
+	}
+
+# TODO: raise warning if quality not 320
+# TODO: download each track's youtube description in a text file ?
+# TODO: faster flattening
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description='Youtube mp3 downloader.')
+	parser.add_argument('-l', '--link', type=str, required=True, help='Youtube Playlist/Track link.')
+	parser.add_argument('-o', '--output', type=str, default='', help='Specify an output directory name.')
+	args = parser.parse_args()
+
+	# If user specified a directory, overwrite the default
+	if args.output!='': 
+		OUTPUT_DIR=args.output
+		YDL_OPTS['outtmpl']=f"{OUTPUT_DIR}/{SIMPLE_FORMAT}"
+
+	# Create the Output Directory
+	os.makedirs(OUTPUT_DIR, exist_ok=True)
+	print(f"Tracks will be downloaded to: {OUTPUT_DIR}\n")
+
+	# Flatten the links if its a playlist
+	links=[]
+	with youtube_dl.YoutubeDL(YDL_OPTS) as ydl: # Get all the individual links
+		result=ydl.extract_info(args.source, download=False)
+		if 'entries' in result: # Playlist
+			for i,item in enumerate(result['entries']):
+				links.append(result['entries'][i]['webpage_url'])
+			print("Flattened the playlist.")
+		else:
+			links=[args.source] # Single track
+
+	print('\nFormatting and Downloading...')
+	for link in links:
+
+		with youtube_dl.YoutubeDL(YDL_OPTS) as ydl:
+
+			# Get information to determine name formatting
+			info_dict=ydl.extract_info(link, download=False)
+			# Check the audio sampling rate
+			if int(info_dict.get('asr', None))<44100:
+				print("Low sampling rate! Skipping.")
+				continue			
+			artist=info_dict.get('artist', None)
+			track=info_dict.get('track', None)
+
+			# Change the formatting if artist and track name is specified
+			if (artist is not None) and (track is not None):
+				form="%(artist)s - %(track)s.%(ext)s"
+				YDL_OPTS['outtmpl']=f"{OUTPUT_DIR}/{form}"
+			else: # Download with the current format
+				try:
+					ydl.download([link])
+				except:
+					print(f"Erron on: {link}")
+				print("")
+				continue
+
+		# Set the new format and Download
+		with youtube_dl.YoutubeDL(YDL_OPTS) as ydl:
+			try:
+				ydl.download([link])
+			except:
+				print(f"Erron on: {link}")
+			print("")
+
+		# Go back to the default format
+		YDL_OPTS['outtmpl']=f"{OUTPUT_DIR}/{SIMPLE_FORMAT}" 
