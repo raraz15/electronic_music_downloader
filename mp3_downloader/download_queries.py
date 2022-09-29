cd9d568ebfca48bb02b5231dee611f44e183e789
Recep Oguz Araz <oguza97@gmail.com>
2022-09-29 11:47:58 +0200

    optional download dir

--- mp3_downloader/download_queries.py
+++ mp3_downloader/download_queries.py
@@ -5,11 +5,11 @@ import argparse
 QUERY_DIR='Queries'
 DOWNLOAD_DIR='Downloads'
 
-# TODO: add doptional download path
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Youtube mp3 downloader from queries.json.')
     parser.add_argument('-q', '--queries', type=str, required=True, help='Directory to the queries.json file.')
+    parser.add_argument('-o', '--output', type=str, default='', help='Specify an output directory name.')
     args = parser.parse_args()
 
     # Load the queries
@@ -21,12 +21,15 @@ if __name__ == '__main__':
     
     # Determine the export directory
     query_name = os.path.splitext(os.path.basename(query_path))[0]
-    download_dir = os.path.join(DOWNLOAD_DIR, query_name)
+    output_dir = os.path.join(DOWNLOAD_DIR, query_name)
+    # If user specified a directory, overwrite the default
+    if args.output!='':
+        output_dir=args.output
 
     # Download all the tracks
     for i, query_dict in enumerate(queries_dict.values()):
         print(f"{i+1}/{len(queries_dict)}")
         if i==0:
-            os.system(f"python mp3_downloader/mp3_downloader.py -l={query_dict['Link']} -o={download_dir} -v")
+            os.system(f"python mp3_downloader/mp3_downloader.py -l={query_dict['Link']} -o={output_dir} -v")
         else:
-            os.system(f"python mp3_downloader/mp3_downloader.py -l={query_dict['Link']} -o={download_dir}")
+            os.system(f"python mp3_downloader/mp3_downloader.py -l={query_dict['Link']} -o={output_dir}")
