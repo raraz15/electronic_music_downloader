85d28f0e758253758f4b85739c185aa6f8c638bb
Recep Oguz Araz <oguza97@gmail.com>
2022-10-02 21:51:05 +0200

    remove moved function

--- emd/scrape_beatport.py
+++ emd/scrape_beatport.py
@@ -129,24 +129,6 @@ if __name__ == '__main__':
         print(f"| {i:>2} | {title:<{max_title_len}} | {artists:<{max_artist_len}} |")
     print("-"*(3+3+2+3+1+max_title_len+max_artist_len))
 
-    # Analysis
-    key_dict,bpm_dict,label_dict,artist_dict=defaultdict(int),defaultdict(int),defaultdict(int),defaultdict(int)
-    remix_dict={'remix': 0, 'original': 0}
-    for track in tracks.values():
-        bpm_dict[track['BPM']] += 1
-        key_dict[track['Key']] += 1
-        label_dict[track['Label']] += 1
-        for artist in track['Artist(s)'].split(','):
-            artist_dict[artist.replace("$$","\$\$")] += 1  # One artist' name included $$ which was bad for OS           
-        if 'Remixer(s)' in track:
-            remix_dict['remix'] += 1
-        else:
-            remix_dict['original'] += 1
-    artist_dict=dict(sorted(artist_dict.items()))
-    key_dict=dict(sorted(key_dict.items()))
-    bpm_dict=dict(sorted(bpm_dict.items()))
-    label_dict=dict(sorted(label_dict.items()))
-
     # Plotting
     analyze_and_plot(tracks,args.save_figure,output_dir,CHART_NAME)
     print(f"Analysis plots exported to: {output_dir}")
