ea15972505141da2553886cf0f71262fa2a15a44
Recep Oguz Araz <oguza97@gmail.com>
2022-10-04 20:10:04 +0200

    analysis, functional scraping

--- emd/scrape_traxsource.py
+++ emd/scrape_traxsource.py
@@ -7,9 +7,11 @@ import argparse
 from bs4 import BeautifulSoup
 
 from scrape_beatport import replace_non_ascii
+from analyze_beatport import analyze_and_plot
 from info import CHARTS_DIR # Default directory
 
 DATE=dt.datetime.strftime(dt.datetime.now(),"%d_%m_%Y")
+PAGE_URL="https://www.traxsource.com"
 
 # TODO: Utils.py maybe
 def duration_str_to_int(duration_str):
@@ -17,14 +19,48 @@ def duration_str_to_int(duration_str):
     duration=int(sec) + 60*int(min)
     return duration
 
-# TODO: Analysis
+def scrape_track_page(url):
+    # Load the track page
+    track_html=requests.get(url).content
+    track_bsObj=BeautifulSoup(track_html,'lxml')
+    image_link=track_bsObj.find("div", {"class": "tr-image"}).find("img")['src']
+    # Header
+    page_head=track_bsObj.find("div", {"class": "page-head"})
+    title=page_head.find("h1", {"class": "title"}).string
+    version=page_head.find("h1", {"class": "version"}).string
+    artists=[t.string for t in page_head.findAll("a", {"class": "com-artists"})]
+    artists=", ".join(artists)
+    remixers=[t.string for t in page_head.findAll("a", {"class": "com-remixers"})]
+    remixers=", ".join(remixers)
+    # Table
+    matches=track_bsObj.find("div", {"class": "tr-details"}).find("table").findAll("td")
+    # The odd elements have the data
+    label,released,duration,genre,key,bpm=[r.string for r in matches[1::2]]
+    # Combine
+    track={'Title': replace_non_ascii(title),
+            'Mix': "" if version is None else version,
+            'Artist(s)': replace_non_ascii(artists),
+            'Remixer(s)': remixers,
+            'Duration(sec)':duration_str_to_int(duration),
+            'Duration(min)': duration,
+            'BPM': int(bpm),
+            'Key': key.split(key[-3:])[0]+" "+key[-3:],
+            'Label': replace_non_ascii(label),
+            'Released': released,
+            'Image Link': image_link,
+            #'Preview': track["preview"]["mp3"]["url"]
+            }
+    return track
+
 # TODO: Preview
 if __name__ == '__main__':
 
-    parser=argparse.ArgumentParser(description='Beatport Top100 Analyzer')
+    parser=argparse.ArgumentParser(description='Traxsource Top100 Analyzer')
     parser.add_argument('-u', '--url', type=str, required=True, help='URL of the Top100 site.')
     parser.add_argument('-o', '--output', type=str, default='', help='Specify an output directory.')
     parser.add_argument('-N', type=int, default=10, help='Number of top entries to display.')
+    parser.add_argument('-a','--analyze', action='store_true', help='Analyze the chart.')
+    parser.add_argument('-s','--save-figure', action='store_true', help='Save the analyzed figures.')
     args=parser.parse_args()
 
     # Get the Genre Name from the URL
@@ -33,10 +69,11 @@ if __name__ == '__main__':
     SIMPLE_NAME=genre.replace('_',' ') # For Plotting and Printing
     print(f"{SIMPLE_NAME} - Top 100")
     
-    # Extract the track information
+    # Load the chart page
     html=requests.get(args.url).content
     bsObj=BeautifulSoup(html, 'lxml')
-    # Find the ids of the tracks
+
+    # Find the Track IDs
     data_trids=[]
     for tag in bsObj.find_all("div"):
         tag_dct=dict(tag.attrs)
@@ -57,46 +94,8 @@ if __name__ == '__main__':
             if 'title' in tag["class"]:
                 url_ext=tag.find("a").attrs['href']
                 break
-        # Reach the track page
-        track_html=requests.get("https://www.traxsource.com"+url_ext).content
-        track_bsObj=BeautifulSoup(track_html,'lxml')
-        # Get track information
-        image_link=track_bsObj.find("div", {"class": "tr-image"}).img['src']
-        page_head=track_bsObj.find("div", {"class": "page-head"})
-        title=page_head.find("h1", {"class": "title"}).string
-        version=page_head.find("h1", {"class": "version"}).string
-        artists=[t.string for t in page_head.findAll("a", {"class": "com-artists"})]
-        artists=", ".join(artists)
-        remixers=[t.string for t in page_head.findAll("a", {"class": "com-remixers"})]
-        remixers=", ".join(remixers)
-        matches=track_bsObj.find("div", {"class": "tr-details"}).find("table").findAll("td")
-        for m1,m2 in zip(matches[::2],matches[1::2]):
-            m1,m2=m1.string,m2.string
-            if m1=="Label:":
-                label=m2
-            elif m1=="Released:":
-                released=m2
-            elif m1=="Length:":
-                duration=m2
-            elif m1=="Genre:":
-                genre=m2
-            elif m1=="Key:":
-                key=m2
-            elif m1=="BPM:":
-                bpm=m2
-        tracks[idx]={'Title': replace_non_ascii(title),
-                        'Mix': "" if version is None else version,
-                        'Artist(s)': replace_non_ascii(artists),
-                        'Remixer(s)': remixers,
-                        'Duration(sec)':duration_str_to_int(duration),
-                        'Duration(min)': duration,
-                        'BPM': int(bpm),
-                        'Key': key.split(key[-3:])[0]+" "+key[-3:],
-                        'Label': replace_non_ascii(label),
-                        'Released': released,
-                        'Image Link': bsObj.find("div", {"class": "tr-image"}).img['src'],
-                        #'Preview': track["preview"]["mp3"]["url"]
-                    }
+        track_URL=PAGE_URL+url_ext
+        tracks[idx]=scrape_track_page(track_URL)
 
     # If user specified a directory, overwrite the default
     if args.output!='':
@@ -122,5 +121,10 @@ if __name__ == '__main__':
         artists=tracks[i]['Artist(s)']
         print(f"| {i:>2} | {title:<{max_title_len}} | {artists:<{max_artist_len}} |")
     print("-"*(3+3+2+3+1+max_title_len+max_artist_len))
+
+    # Plotting
+    if args.analyze:
+        analyze_and_plot(tracks,args.save_figure,output_dir,CHART_NAME)
+        print(f"Analysis plots exported to: {output_dir}")
 
     print("Done!")
