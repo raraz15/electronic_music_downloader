86dc12c043c2ff33c8f424279eb812527b11963c
Recep Oguz Araz <oguza97@gmail.com>
2022-10-10 16:45:35 +0200

    / bug

--- emd/beatport/searcher.py
+++ emd/beatport/searcher.py
@@ -2,7 +2,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-HOME_PAGE="https://www.beatport.com/"
+HOME_PAGE="https://www.beatport.com"
 
 def make_beatport_query(query_str):
     """Returns the top url from a beatport search. query_str should contain
@@ -17,12 +17,12 @@ def make_beatport_query(query_str):
         query_str=re.sub(r"\s","+", query_str)
         query_str=re.sub(r",","%2C", query_str)
         # Form the query url
-        url=f"{HOME_PAGE}search?q={query_str}"
+        url=f"{HOME_PAGE}/search?q={query_str}"
         html=requests.get(url).content
         bsObj=BeautifulSoup(html,'lxml')
         t=bsObj.find("li",{"class":"bucket-item ec-item track"}) # List containing the tracks
         track_url=t.find("div",{"class": "buk-track-meta-parent"}).find("a")["href"]
-        return track_url
+        return HOME_PAGE+"/"+track_url
     except:
         print(f"Error on: {query_str}")
         return ""
